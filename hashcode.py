d72625ee99de1f770e75c1b64bc4d4256ee91315
BLARD Justin <justin.blard@epitech.eu>
2021-02-25 19:14:47 +0100

    [INIT] Project hashcode

---
+++ hashcode.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+from sys import exit, argv
+from src.main import main
+
+if __name__ == "__main__":
+    exit(main(argv))
---
+++ src/main.py
@@ -0,0 +1,9 @@
+##
+## EPITECH PROJECT, 2021
+## HashCode2021
+## File description:
+## value
+##
+
+def main(arguments):
+    return 0
